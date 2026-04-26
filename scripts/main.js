9946e4363344ca3434beac0a8a884f8671073f32
sopranoo0 <greg-lutzka@yandex.ru>
2026-04-26 14:08:31 +0300

    add: reviews slider

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+        
+    if ($('.js-swiperStaffs').length) {
+        new Swiper(".js-swiperStaffs", {
+            // slidesPerView: "auto",
+            freeMode: true,
+            slidesPerView: 3,
+            spaceBetween: 30,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                },
+                992: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+            }
+        });
+    }
+
+});
